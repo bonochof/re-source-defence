a838bbca36f2a4ed4b8345977565ab6f2ef1ccc2
bonochof <ryos427@yahoo.co.jp>
2018-10-29 01:19:01 +0900

    add director

---
+++ director.rb
@@ -0,0 +1,10 @@
+class Director
+  def initialize
+  end
+
+  def input
+  end
+
+  def play
+  end
+end
--- main.rb
+++ main.rb
@@ -1,5 +1,15 @@
 require 'dxruby'
 
-Window.loop do
+require_relative 'director'
 
+Window.caption = "RE:source Defence"
+#Window.width = 800
+#Window.height = 600
+
+director = Director.new
+
+Window.loop do
+  break if Input.keyPush?(K_ESCAPE)
+  director.input
+  director.play
 end
