78960ded5b21cc0c497efb1ca596dbc26bd71e9c
yagura46gin37k <jshogi12-taisa@yahoo.co.jp>
2018-10-29 23:21:21 +0900

    rename map.rb to mapdata.rb

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ require_relative 'director'
 require_relative 'menu'
 require_relative 'cell'
 require_relative 'mapfactory'
-require_relative 'map'
+require_relative 'mapdata'
 
 Window.caption = "RE:source Defence"
 #Window.width = 800
