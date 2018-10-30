567da7e6ef35f4974b5e48919f6e3cfee2b8ff0f
yagura46gin37k <jshogi12-taisa@yahoo.co.jp>
2018-10-30 21:41:22 +0900

    change timing of Image.new and Image.load

--- card.rb
+++ card.rb
@@ -1,18 +1,19 @@
 class Card < Sprite
   CARD_WIDTH = 100
   CARD_HEIGHT = 160
+  @@image = Image.new(CARD_WIDTH, CARD_HEIGHT, C_BLUE)
+  @@nameFont = Font.new(24, "Consolas")
+  @@textFont = Font.new(16, "Consolas")
 
   def initialize(x, y, name, text)
     @name = name
     @text = text
-    @nameFont = Font.new(24, "Consolas")
-    @textFont = Font.new(16, "Consolas")
-    super(x, y, Image.new(CARD_WIDTH, CARD_HEIGHT, C_BLUE))
+    super(x, y, @@image)
   end
 
   def draw
     super
-    Window.draw_font(self.x, self.y, @name, @nameFont, :color=>C_WHITE)
-    Window.draw_font(self.x, self.y + 50, @text, @textFont, :color=>C_WHITE)
+    Window.draw_font(self.x, self.y, @name, @@nameFont, :color=>C_WHITE)
+    Window.draw_font(self.x, self.y + 50, @text, @@textFont, :color=>C_WHITE)
   end
 end
--- mapdata.rb
+++ mapdata.rb
@@ -1,12 +1,13 @@
 class MapData
+  @@mapImages = Image.load_tiles("./img/map.bmp", 8, 8, true)
+
   def initialize(x, y, ary_tileId)
-    @mapImages = Image.load_tiles("./img/map.bmp", 8, 8, true)
     @mapTiles = []
 
     ary_tileId.each_with_index do |ary, i|
       lines = []
       ary.each_with_index do |elm, j|
-        lines << Sprite.new(x + j * 32, y + i * 32, @mapImages[elm])
+        lines << Sprite.new(x + j * 32, y + i * 32, @@mapImages[elm])
       end
       @mapTiles << lines
     end
