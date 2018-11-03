b6d2e3632b621726df884526663f840a133b80a0
yagura46gin37k <jshogi12-taisa@yahoo.co.jp>
2018-11-03 10:10:53 +0900

    change card to make new line for long text

--- card.rb
+++ card.rb
@@ -10,8 +10,11 @@ class Card < Sprite
     @card_image = Image.new(CARD_WIDTH, CARD_HEIGHT, color1)
     @card_image.box_fill(5, 5, CARD_WIDTH - 5, NAME_SIZE + 5, color2)
     @card_image.box_fill(5, NAME_SIZE + 10, CARD_WIDTH - 5, CARD_HEIGHT - 5, color2)
-    @card_image.draw_font_ex(CARD_WIDTH / 2 - @@name_font.get_width(name) / 2, 5, name, @@name_font, :color=>C_BLACK)
-    @card_image.draw_font_ex(5, NAME_SIZE + 10, text, @@text_font, :color=>C_BLACK)
+    @card_image.draw_font_ex(CARD_WIDTH / 2 - @@name_font.get_width(name) / 2, 5, name, @@name_font, :color => C_BLACK)
+
+    text.scan(/.{1,#{11}}/).each_with_index do |str, i|
+      @card_image.draw_font_ex(5, NAME_SIZE + 10 + i * TEXT_SIZE, str, @@text_font, :color => C_BLACK)
+    end
     super(x, y, @card_image)
   end
 end
