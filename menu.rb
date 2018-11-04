01cddac4ac8d6c0c70979b948d7812c54624b0b7
bonochof <ryos427@yahoo.co.jp>
2018-11-05 00:55:56 +0900

    fix var type

--- menu.rb
+++ menu.rb
@@ -1,14 +1,14 @@
 class Menu < Sprite
   FONT_SIZE = 24
+  @@font = Font.new(FONT_SIZE, "Consolas", :weight=>true)
 
   def initialize(x, y, color, str)
     @str = str
-    @font = Font.new(FONT_SIZE, "Consolas", :weight=>true)
     super(x, y, Image.new(100, 50, color))
   end
 
   def draw
     super
-    Window.draw_font(self.x, self.y, @str, @font)
+    Window.draw_font(self.x, self.y, @str, @@font)
   end
 end
