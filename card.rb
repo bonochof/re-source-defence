4e5d780ccf977560ac28c5a28b03067c789f2a19
yagura46gin37k <jshogi12-taisa@yahoo.co.jp>
2018-11-06 01:59:11 +0900

    add CardInfo(module) into card.rb

--- card.rb
+++ card.rb
@@ -1,3 +1,14 @@
+module CardInfo
+  #INFO = [name, text, effect]
+  MARKET = ["market", "+1 gold", Proc.new do
+    Kingdom.gold += 1
+  end]
+
+  TEST = ["test", "01234567890this is newline test", Proc.new do
+    p "card effect test!"
+  end]
+end
+
 class Card < Sprite
   CARD_WIDTH = 100
   CARD_HEIGHT = 140
@@ -6,19 +17,23 @@ class Card < Sprite
   @@name_font = Font.new(NAME_SIZE, "Consolas")
   @@text_font = Font.new(TEXT_SIZE, "Consolas")
 
-  def initialize(x, y, name, text, color1: C_BLUE, color2: C_WHITE)
+  def initialize(x, y, name = CardInfo::TEST, color1: C_BLUE, color2: C_WHITE)
+    @name = name[0]
+    @text = name[1]
+    @effect = name[2]
+
     @card_image = Image.new(CARD_WIDTH, CARD_HEIGHT, color1)
     @card_image.box_fill(5, 5, CARD_WIDTH - 5, NAME_SIZE + 5, color2)
     @card_image.box_fill(5, NAME_SIZE + 10, CARD_WIDTH - 5, CARD_HEIGHT - 5, color2)
-    @card_image.draw_font_ex(CARD_WIDTH / 2 - @@name_font.get_width(name) / 2, 5, name, @@name_font, :color => C_BLACK)
+    @card_image.draw_font_ex(CARD_WIDTH / 2 - @@name_font.get_width(@name) / 2, 5, @name, @@name_font, :color => C_BLACK)
 
-    text.scan(/.{1,#{11}}/).each_with_index do |str, i|
+    @text.scan(/.{1,#{11}}/).each_with_index do |str, i|
       @card_image.draw_font_ex(5, NAME_SIZE + 10 + i * TEXT_SIZE, str, @@text_font, :color => C_BLACK)
     end
     super(x, y, @card_image)
   end
 
   def use
-    yield
+    @effect.call
   end
 end
